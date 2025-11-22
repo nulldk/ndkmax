9be3ccb87d90ecb2419dddaa40dc619c526bafb0
NDK <ebc131103@gmail.com>
2025-11-22 15:41:39 +0100

    Versión cambiada

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv, dotenv_values
 # --- Constantes de configuración ---
 load_dotenv()
 
-VERSION = "1.0.0"
+VERSION = "1.0.1"
 IS_DEV = os.getenv("NODE_ENV") == "development"
 ROOT_PATH = os.environ.get("ROOT_PATH", "")
 
